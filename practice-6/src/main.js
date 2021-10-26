bed8ec85ca1ffe39f309c9703470cccdd54be9ba
trongtran <quangtrong19981@gmail.com>
2021-10-26 13:56:37 +0700

    Add practice-6

---
+++ practice-6/src/main.js
@@ -0,0 +1,15 @@
+let newNum = prompt("Please input a number larger than or equal 3");
+let counti = 0;
+let countj = 0
+
+while ( newNum < 3) {
+  alert("Please retry again!");
+  newNum = prompt("Please input a number larger than or equal 3");
+}
+
+for (counti = 1; counti <= newNum; counti++) {
+  for (countj = 1; countj <= counti; countj++){
+  document.write("*");
+  }
+  document.write("<br/>");
+}
