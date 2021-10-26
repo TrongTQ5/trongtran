f05108633ce178f7717f41b6fb8f92304d24912e
trongtran <quangtrong19981@gmail.com>
2021-10-26 13:18:48 +0700

    Add practice-4

---
+++ practice-4/src/main.js
@@ -0,0 +1,66 @@
+// Input day
+let newDay = prompt("Please input a number from 1 to 7");
+
+switch (newDay) {
+  case '1': document.write("Sunday <br/>");
+  break;
+
+  case '2': document.write("Monday <br/>");
+  break;
+
+  case '3': document.write("Tuesday <br/>");
+  break;
+
+  case '4': document.write("Wednesday <br/>");
+  break;
+
+  case '5': document.write("Thursday <br/>");
+  break;
+
+  case '6': document.write("Friday <br/>");
+  break;
+
+  case '7': document.write("Saturday <br/>");
+  break;
+}
+
+// Input months
+let newMonth = prompt("Please input a number from 1 to 12");
+
+switch (newMonth) {
+  case '1': document.write("January");
+  break;
+
+  case '2': document.write("February");
+  break;
+
+  case '3': document.write("March");
+  break;
+
+  case '4': document.write("April");
+  break;
+
+  case '5': document.write("May");
+  break;
+
+  case '6': document.write("June");
+  break;
+
+  case '7': document.write("July");
+  break;
+
+  case '8': document.write("August");
+  break;
+
+  case '9': document.write("September");
+  break;
+
+  case '10': document.write("October");
+  break;
+
+  case '11': document.write("November");
+  break;
+
+  case '12': document.write("December");
+  break;
+}
