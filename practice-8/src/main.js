4998f002110817654f49a9d38c393aab128a929b
trongtran <quangtrong19981@gmail.com>
2021-11-02 10:54:28 +0700

    Update practice-8 and registration form

--- practice-8/src/main.js
+++ practice-8/src/main.js
@@ -1,11 +1,11 @@
-function User(firstName, lastName, age, gender, school, address) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.gender = gender;
-  this.school = school;
-  this.address = address;
-}
+// function User(firstName, lastName, age, gender, school, address) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.age = age;
+//   this.gender = gender;
+//   this.school = school;
+//   this.address = address;
+// }
 
 let firstName = prompt("Input your first name");
 let lastName = prompt("Input your last name");
@@ -14,3 +14,50 @@ let genderSt = prompt("Input your Gender");
 let schoolSt = prompt("Input your School");
 let addressSt = prompt("Input your Address");
 
+
+while (firstName.length < 3) {
+  firstName = prompt("Please input your first name, first name must larger than or equal 3 characters");
+}
+
+while (lastName.length < 3) {
+  lastName = prompt("Please input your last name, last name must larger than or equal 3 characters");
+}
+
+while (addressSt.length > 40) {
+  addressSt = prompt("Please input your Address, Address does not exceed 40 characters");
+}
+
+// let user1 =new User(firstName, lastName, ageSt, genderSt, schoolSt, addressSt);
+// console.log(user1);
+
+let tableUser = document.querySelector(`#tableUser`);
+console.log(tableUser);
+tableUser.innerHTML =
+  `<table>
+  <tr>
+    <th>First name</th>
+    <td>${firstName}</td>
+  </tr>
+  <tr>
+    <th>Last name</th>
+    <td>${lastName}</td>
+  </tr>
+  <tr>
+    <th>Age</th>
+    <td>${ageSt}</td>
+  </tr>
+  <tr>
+    <th>Gender</th>
+    <td>${genderSt}</td>
+  </tr>
+  <tr>
+    <th>School</th>
+  <td>${schoolSt}</td>
+  </tr>
+  <tr>
+    <th>Address</th>
+    <td>${addressSt}</td>
+  </tr>
+</table>
+  `
+;
